07b7d964b5ad50391e93395be975e194ab18b6c2
João Marllon Oliveira Costa Lessa <jmarllonocl2008@gmail.com>
2025-01-06 22:03:06 -0300

    modificações necessárias

--- Exercícios em Python (mundo 1)/ex009.py
+++ Exercícios em Python (mundo 1)/ex009.py
@@ -1,4 +1,13 @@
 numero = int(input("Digite um número: "))
 print(f"Tabuada do: {numero}")
-for i in range(1,11):
-    print(f"{i} * {numero} = {i * numero}")
+
+print('{} * 1 = {}'.format(numero, numero * 1))
+print('{} * 2 = {}'.format(numero, numero * 2))
+print('{} * 3 = {}'.format(numero, numero * 3))
+print('{} * 4 = {}'.format(numero, numero * 4))
+print('{} * 5 = {}'.format(numero, numero * 5))
+print('{} * 6 = {}'.format(numero, numero * 6))
+print('{} * 7 = {}'.format(numero, numero * 7))
+print('{} * 8 = {}'.format(numero, numero * 8))
+print('{} * 9 = {}'.format(numero, numero * 9))
+print('{} * 10 = {}'.format(numero, numero * 10))
---
+++ Exercícios em Python (mundo 2)/ex046.py
@@ -0,0 +1,7 @@
+from time import sleep
+
+print('Contagem regresssiva!!!')
+for i in range(10, -1, -1):
+    print('-> {}'.format(i))
+    sleep(1)
+print('💥💥💥💥💥💥💥💥💥💥💥💥💥💥')
